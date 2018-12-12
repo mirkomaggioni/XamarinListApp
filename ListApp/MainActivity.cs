7792b512f776886b50dcd849a904a5e394397246
Mirko Maggioni <mirko.maggioni@sintel.net>
2018-12-12 16:13:59 +0100

    authentication service is implemented

--- ListApp/MainActivity.cs
+++ ListApp/MainActivity.cs
@@ -1,18 +1,38 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
 using Android.App;
 using Android.Content;
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using ListApp.BusinessObjects;
+using ListApp.Services;
 
 namespace ListApp
 {
 	[Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+		AuthenticationService _authenticationService;
 
-        protected override void OnCreate(Bundle savedInstanceState)
+		public MainActivity()
+		{
+			var serializer = new XmlSerializer(typeof(AppSettings));
+			var assembly = IntrospectionExtensions.GetTypeInfo(typeof(AuthenticationService)).Assembly;
+			var stream = assembly.GetManifestResourceStream("ListApp.AppSettings.xml");
+
+			using (var reader = new StreamReader(stream))
+			{
+				var settings = (AppSettings)serializer.Deserialize(reader);
+				_authenticationService = new AuthenticationService(settings.ServerAppUrl);
+			}
+		}
+
+		protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             SetContentView(Resource.Layout.activity_main);
@@ -21,7 +41,7 @@ namespace ListApp
             SetSupportActionBar(toolbar);
 
 			var button = FindViewById<Button>(Resource.Id.btnLogin);
-            button.Click += LoginOnClick;
+            button.Click += async (sender, e) => await LoginOnClickAsync(sender, e);
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -41,12 +61,18 @@ namespace ListApp
             return base.OnOptionsItemSelected(item);
         }
 
-        private void LoginOnClick(object sender, EventArgs eventArgs)
+        private async Task LoginOnClickAsync(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
-			var intent = new Intent(this, typeof(PersonsListActivity));
-			StartActivity(intent);
+			var fldUsername = FindViewById<TextView>(Resource.Id.fldUsername);
+			var fldPassword = FindViewById<TextView>(Resource.Id.fldPassword);
+			var credentials = new UserCredentials() { Username = fldUsername.Text, Password = fldPassword.Text };
+
+			if (await _authenticationService.LogonAsync(credentials))
+			{
+				View view = (View)sender;
+				var intent = new Intent(this, typeof(PersonsListActivity));
+				StartActivity(intent);
+			}
         }
 	}
 }
-
---
+++ ListApp/Services/AuthenticationService.cs
@@ -0,0 +1,36 @@
+﻿using ListApp.BusinessObjects;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Xamarin.Android.Net;
+
+namespace ListApp.Services
+{
+	public class AuthenticationService
+	{
+		private readonly string _baseUrl;
+
+		public AuthenticationService(string baseUrl)
+		{
+			_baseUrl = baseUrl;
+		}
+
+		public async Task<bool> LogonAsync(UserCredentials credentials)
+		{
+			var client = new HttpClient(new AndroidClientHandler());
+			client.BaseAddress = new System.Uri(_baseUrl);
+
+			var content = new FormUrlEncodedContent(new[]
+			{
+				new KeyValuePair<string, string>("grant_type", "password"),
+				new KeyValuePair<string, string>("dominio", "Lombardia"),
+				new KeyValuePair<string, string>("username", credentials.Username),
+				new KeyValuePair<string, string>("password", credentials.Password)
+			});
+
+			var response = await client.PostAsync("/oauth/token", content);
+
+			return response.IsSuccessStatusCode;
+		}
+	}
+}
