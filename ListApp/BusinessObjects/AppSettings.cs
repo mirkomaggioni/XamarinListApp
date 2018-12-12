fbae2b543f94cb944c6ca8af68e4fa48d210ed3e
Mirko Maggioni <mirko.maggioni@sintel.net>
2018-12-12 16:10:16 +0100

    appsettings is added

---
+++ ListApp/BusinessObjects/AppSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace ListApp.BusinessObjects
+{
+	[Serializable()]
+	[XmlRoot("settings")]
+	public class AppSettings
+	{
+		[XmlElement("serverAppUrl")]
+		public string ServerAppUrl { get; set; }
+	}
+}
